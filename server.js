7cc1cf04c3b66ca3d1b74f13675811f5345a6f84
2337481b-oss <2337481b@umich.mx>
2026-04-08 18:58:47 -0600

    mostrar frontend

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const path = require("path");
 const multer = require("multer");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
@@ -8,6 +9,7 @@ const jwt = require("jsonwebtoken");
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use(express.static(path.join(__dirname, "public")));
 app.use("/uploads", express.static("uploads"));
 
 // 🔌 CONEXIÓN A MONGODB
